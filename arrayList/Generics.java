4985d3f57a5ab73ec2fd3523a8d14fc7382ca89c
dvilChandra <57448981+dvilChandra@users.noreply.github.com>
2020-10-28 19:05:07 +0530

    Update Generics.java

--- arrayList/Generics.java
+++ arrayList/Generics.java
@@ -12,6 +12,19 @@ public class Generics<X,Y,Z> {
 	}
 	public void print() {
 		System.out.println(x +" ,"+ y +"and " + z);
+		// hi debsisu this is devil 
+		public static void main (String[] args) 
+    { 
+        Stack<Integer> stack = new Stack<Integer>(); 
+  
+        stack_push(stack); 
+        stack_pop(stack); 
+        stack_push(stack); 
+        stack_peek(stack); 
+        stack_search(stack, 2); 
+        stack_search(stack, 6); 
+    } 
+} 
 	}
 	
 }
